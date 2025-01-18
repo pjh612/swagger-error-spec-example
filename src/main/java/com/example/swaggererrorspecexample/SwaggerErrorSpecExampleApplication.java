48aadad7c5036a53cc3bd00a945c7b2db557587a
pjh612 <pjh_jn@naver.com>
2025-01-18 12:35:32 +0900

    first commit

---
+++ src/main/java/com/example/swaggererrorspecexample/SwaggerErrorSpecExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.swaggererrorspecexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerErrorSpecExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwaggerErrorSpecExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/swaggererrorspecexample/SwaggerErrorSpecExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.swaggererrorspecexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerErrorSpecExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
